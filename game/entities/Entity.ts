be79cb4a99b5c14acfc42aaae1fada197644f737
grobo <grobolsek.matic04@gmail.com>
2024-12-15 22:34:06 +0100

    entity will take hitboxes position since entity will have only one hitbox meaning that it doesn't need to have separated position

--- game/entities/Entity.ts
+++ game/entities/Entity.ts
@@ -6,10 +6,10 @@ class Entity {
     hitbox: Hitbox;
     position: Vector3;
 
-    constructor(health: number, hitbox: Hitbox, position: Vector3) {
+    constructor(health: number, hitbox: Hitbox) {
         this.health = health;
         this.hitbox = hitbox;
-        this.position = position;
+        this.position = this.hitbox.center;
     }
 
     updatePosition(vector: Vector3): void {
