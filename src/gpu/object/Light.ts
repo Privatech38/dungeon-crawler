a31599ae6fd1cf9c281877f744651d367b3179ff
Privatech <ziga.slatnar.stagar@gmail.com>
2025-11-20 10:15:23 +0100

    Update description

--- src/gpu/object/Light.ts
+++ src/gpu/object/Light.ts
@@ -1,6 +1,9 @@
 /**
- * Class representing JSON structure for a Light as specified by Khronos Group in glTF 2.0.
+ * Class representing JSON structure for a Light as specified by Khronos Group in glTF 2.0.<br>
  * https://github.com/KhronosGroup/glTF/blob/main/extensions/2.0/Khronos/KHR_lights_punctual/README.md
+ *
+ * There is also an index field <code>light</code>, but it is not included here as it is consumed
+ * when applying the light to a node.
  */
 interface Light {
     name?: string;
